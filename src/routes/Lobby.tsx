792a3f4be9b6c731649923f548b0eb27fadf5cfb
nadav5199 <nadavpassal@gmail.com>
2025-04-21 14:48:40 +0300

    removed seedData

--- src/routes/Lobby.tsx
+++ src/routes/Lobby.tsx
@@ -1,24 +1,12 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { seedCodeBlocks } from "../../amplify/data/seedData";
 import CodeBlocksCard from "../components/CodeBlocksCard";
 
 const client = generateClient<Schema>();
 
 function Lobby() {
     const [codeBlocks, setCodeBlocks] = useState<Array<Schema["CodeBlock"]["type"]>>([]);
-
-    useEffect(() => {
-        const initializeData = async () => {
-            try {
-                await seedCodeBlocks();
-            } catch (error) {
-                console.error('Error seeding CodeBlock table:', error);
-            }
-        };
-        initializeData();
-    }, []);
 
     useEffect(() => {
         client.models.CodeBlock.observeQuery().subscribe({
